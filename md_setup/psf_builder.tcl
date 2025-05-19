28b2a52ebf5ee995ded4855ceb2835fbe4cd59a4
BlueSky2311 <anhmai567@outlook.com>
2025-05-19 15:08:26 +0900

    have no ideas, back psf_builder to previous version

--- md_setup/psf_builder.tcl
+++ md_setup/psf_builder.tcl
@@ -1,22 +1,12 @@
 # PSF Builder Script
-#Usage: vmd -dispdev text -e psf_builder.tcl <.pdb file>
-
-# Get input PDB file from command-line argument
-set pdb_file [lindex $argv 0]
-# Extract base name (remove path and extension)
-set pdb_base [file rootname [file tail $pdb_file]]
-# Use first 4 characters for segment name
-set seg_name [string range $pdb_base 0 3]
-
+#Usage: vmd -dispdev text -e psf_builder.tcl
 # Create working directory; remove old output files
-mkdir -p output
-set out_protein "output/${pdb_base}_protein.pdb"
-set out_hetatm "output/${pdb_base}_HETATM.pdb"
-rm -f $out_protein $out_hetatm
+#mkdir -p output
+#rm -f output/output_protein.pdb
 
-# Remove HETATM lines from input PDB
-exec grep -v HETATM $pdb_file > $out_protein
-exec grep -v ATOM $pdb_file > $out_hetatm
+# Remove HETATM lines from input.pdb
+#grep -v 'HETATM' 6pti.pdb > output/output_protein.pdb
+#grep -v 'ATOM' 6pti.pdb > output/output_HETATM.pdb
 
 # Require packages
 package require psfgen
@@ -40,23 +30,24 @@ foreach topo_file $topology_files {
 #change HIS to HSD
 pdbalias residue HIS HSD
 
-# Build protein segment
-segment $seg_name {
-    pdb $out_protein
+#Build protein segment
+# Segment name max 4 characters
+segment 1BYI {
+    pdb output/output_protein.pdb
 }
 
 #patch protein segment: patch [list] <patch residue name> <segid:resid>
-#patch DISU $seg_name:5 $seg_name:55
+#patch DISU BPTI:5 BPTI:55
 
 #Read protein coordinates from PDB file
 pdbalias atom ILE CD1 CD
-coordpdb $out_protein $seg_name
+coordpdb output/output_protein.pdb 1BYI
 
 # Build missing atoms and guess missing coordinates
 guesscoord
 
 # Write the PSF and PDB files
-writepsf ${seg_name}.psf
-writepdb ${seg_name}.pdb
+writepsf 1BYI.psf
+writepdb 1BYI.pdb
 
 exit
