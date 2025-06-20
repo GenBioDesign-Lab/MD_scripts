9bd49f65d02aca1a597a5b1ec0471147742e5e2d
mdanh <anhmai567@outlook.com>
2025-06-20 10:13:17 +0900

    forgot to update content in the script in the previous commit.

--- md_analysis/combine_dcd.sh
+++ md_analysis/combine_dcd.sh
@@ -1,7 +1,8 @@
 #Put this script into the namd folder
+#VMD in HPC can't call the package catdcd in VMD, so download and specify the catdcd manually: https://www.ks.uiuc.edu/Development/MDTools/catdcd/files/catdcd-4.0b.tar.gz
 
 for {set i 1} {$i <= 6} {incr i} {
-    catdcd -o eq_all.dcd eq$i/final_eq$i.dcd
+    /data01/genbiolab/mdanh/data/CatDCD/LINUX/bin/catdcd4.0/catdcd -o eq_all.dcd eq$i/final_eq$i.dcd
 }
 
 exit
