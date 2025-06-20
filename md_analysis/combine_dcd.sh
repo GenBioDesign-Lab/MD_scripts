f2eb1069c737531dc4ff7ffab3c1172075d8ad5f
mdanh <anhmai567@outlook.com>
2025-06-20 10:22:47 +0900

    HPC don't support 32bit lib. Change to 64bit

--- md_analysis/combine_dcd.sh
+++ md_analysis/combine_dcd.sh
@@ -1,8 +1,6 @@
 #Put this script into the namd folder
 #VMD in HPC can't call the package catdcd in VMD, so download and specify the catdcd manually: https://www.ks.uiuc.edu/Development/MDTools/catdcd/files/catdcd-4.0b.tar.gz
 
-for {set i 1} {$i <= 6} {incr i} {
-    /data01/genbiolab/mdanh/data/CatDCD/LINUX/bin/catdcd4.0/catdcd -o eq_all.dcd eq$i/final_eq$i.dcd
-}
-
-exit
+for i in {1..6}; do
+    /data01/genbiolab/mdanh/data/CatDCD/LINUXAMD64/bin/catdcd4.0/catdcd -o eq_all.dcd eq$i/final_eq$i.dcd
+done
