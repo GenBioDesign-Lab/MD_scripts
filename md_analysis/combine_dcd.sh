c6e80dd76a6450da9300f5a03fb6e8ba4de86dc5
mdanh <anhmai567@outlook.com>
2025-06-23 18:08:47 +0900

    minor changes, re-order files

--- md_analysis/combine_dcd.sh
+++ md_analysis/combine_dcd.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Put this script into the namd folder
 #VMD in HPC can't call the package catdcd in VMD, so download and specify the catdcd manually: https://www.ks.uiuc.edu/Development/MDTools/catdcd/files/catdcd-4.0b.tar.gz
 
