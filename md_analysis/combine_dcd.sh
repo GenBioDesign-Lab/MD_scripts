4aaef6611303fea394715afb040f4015093dbde7
mdanh <anhmai567@outlook.com>
2025-06-26 11:13:25 +0900

    corrected script for combine multiple dcd

--- md_analysis/combine_dcd.sh
+++ md_analysis/combine_dcd.sh
@@ -2,6 +2,14 @@
 #Put this script into the namd folder
 #VMD in HPC can't call the package catdcd in VMD, so download and specify the catdcd manually: https://www.ks.uiuc.edu/Development/MDTools/catdcd/files/catdcd-4.0b.tar.gz
 
-for i in {1..6}; do
-    /data01/genbiolab/mdanh/data/CatDCD/LINUXAMD64/bin/catdcd4.0/catdcd -o eq_all.dcd eq$i/final_eq$i.dcd
+# Set the number of equilibration phases
+NUM_PHASES=6
+
+# Build the list of input files
+INPUT_FILES=""
+for i in $(seq 1 $NUM_PHASES); do
+    INPUT_FILES="$INPUT_FILES eq$i/final_eq$i.dcd"
 done
+
+# Run catdcd with all input files
+/data01/genbiolab/mdanh/data/CatDCD/LINUXAMD64/bin/catdcd4.0/catdcd -o eq_all.dcd $INPUT_FILES
