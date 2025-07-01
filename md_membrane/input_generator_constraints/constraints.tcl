f2269331e61fc5b52bf43d236b2a373812fb4de9
mdanh <anhmai567@outlook.com>
2025-07-01 12:08:57 +0900

    update tcl to set k on cnt

--- md_membrane/input_generator_constraints/constraints.tcl
+++ md_membrane/input_generator_constraints/constraints.tcl
@@ -2,6 +2,11 @@
 mol new complex.psf
 mol addfile complex.pdb
 
+#set all atom beta =0
+
+set all [atomselect top all]
+$all set beta 0.0
+
 #set k value
 
 set k 2.5 ;# Kcal/mol/A^2
@@ -13,6 +18,6 @@ set cnt [atomselect top "segname CNT"]
 #store k value in B column
 $cnt set beta $k
 
-$cnt writepdb cnt_constraints.pdb
+$all writepdb cnt_constraints.pdb
 
 quit
